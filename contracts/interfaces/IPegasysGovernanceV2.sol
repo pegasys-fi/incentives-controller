2f64355f7e77604180d02c6fc1df1b9af40dc6e9
David Potolski Lafetá <davidpotolskilafeta@gmail.com>
2024-03-12 15:20:00 -0300

    Adds Pegasys Governance

--- contracts/interfaces/IPegasysGovernanceV2.sol
+++ contracts/interfaces/IPegasysGovernanceV2.sol
@@ -1,4 +1,4 @@
 // SPDX-License-Identifier: agpl-3.0
 pragma solidity 0.7.5;
 
-import '@aave/governance-v2/contracts/interfaces/IAaveGovernanceV2.sol';
+import '@pollum-io/pegasys-governance-v2/contracts/interfaces/IPegasysGovernanceV2.sol';
